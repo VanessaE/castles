f0c9a0e85eb085d872892b09c6d0741c29dc8bcb
FaceDeer <derksenmobile@gmail.com>
2017-02-15 01:15:54 -0700

    split out workbench

--- init.lua
+++ init.lua
@@ -12,8 +12,9 @@ function castle.fix_rotation_nsew(pos, placer, itemstack, pointed_thing)
 end
 
 dofile(minetest.get_modpath("castle").."/tapestry.lua")
-dofile(minetest.get_modpath("castle").."/town_item.lua")
 dofile(minetest.get_modpath("castle").."/shields_decor.lua")
+
+dofile(minetest.get_modpath("castle").."/town_item.lua")
 dofile(minetest.get_modpath("castle").."/orbs.lua")
 dofile(minetest.get_modpath("castle").."/rope.lua")
 
@@ -23,8 +24,11 @@ dofile(minetest.get_modpath("castle").."/battleaxe.lua")
 dofile(minetest.get_modpath("castle").."/ironbound_chest.lua")
 dofile(minetest.get_modpath("castle").."/crate.lua")
 
+dofile(minetest.get_modpath("castle").."/workbench.lua")
+
 dofile(minetest.get_modpath("castle").."/castle_stonewall.lua")
 dofile(minetest.get_modpath("castle").."/castle_structure.lua")
+
 dofile(minetest.get_modpath("castle").."/castle_gates.lua")
 
 
