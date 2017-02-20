482652914bfa1c9571fdf1ea867a04590449451d
FaceDeer <derksenmobile@gmail.com>
2017-02-19 18:22:38 -0700

    There's still refinement to be done, but we have hinges!

--- castle_gates.lua
+++ castle_gates.lua
@@ -80,6 +80,33 @@ local get_dirs = function(facedir)
 	return dirs
 end
 
+local interpret_hinge = function(hinge_def, pos, node_dirs)
+	local axis_dir = node_dirs[hinge_def.axis]
+	local axis
+	if axis_dir.x ~= 0 then
+		axis = "x"
+	elseif axis_dir.y ~= 0 then
+		axis = "y"
+	else
+		axis = "z"
+	end
+	
+	local placement
+	if type(hinge_def.offset) == "string" then
+		placement = vector.add(pos, node_dirs[hinge_def.offset])
+	elseif type(hinge_def.offset) == "table" then
+		placement = vector.new(0,0,0)
+		for _, val in pairs(hinge_def.offset) do
+			placement = vector.add(placement, node_dirs[val])
+		end
+		placement = vector.add(pos, vector.normalize(placement))
+	else
+		placement = pos
+	end
+
+	return axis, placement
+end
+
 
 dofile(minetest.get_modpath("castle").."/class_pointset.lua")
 
@@ -91,10 +118,8 @@ local get_door_layout = function(pos, facedir, player)
 	local door = {}
 
 	door.all = {}
-	door.hinges = {}
 	door.contains_protected_node = false
 	door.directions = get_dirs(facedir)
-	door.can_slide = {}
 	door.previous_move = minetest.get_meta(pos):get_string("previous_move")
 
 	-- temporary pointsets used while searching
@@ -125,11 +150,23 @@ local get_door_layout = function(pos, facedir, player)
 			local test_node_dirs = get_dirs(test_node.param2)
 			local coplanar = vector.equals(test_node_dirs.back, door.directions.back)
 
-			if coplanar and (test_node_def.groups.castle_gate or test_node_def.groups.castle_gate_hinge) then
+			if coplanar and test_node_def.groups.castle_gate then
 				local entry = {["pos"] = test_pos, ["node"] = test_node}
 				table.insert(door.all, entry)
-				if test_node_def.groups.castle_gate_hinge then
-					table.insert(door.hinges, entry)
+				if test_node_def._gate_hinge ~= nil then
+					local axis, placement = interpret_hinge(test_node_def._gate_hinge, test_pos, test_node_dirs)
+					if door.hinge == nil then
+						door.hinge = {axis=axis, placement=placement}
+					elseif door.hinge.axis ~= axis then
+						return nil -- Misaligned hinge axes, door cannot rotate.
+					else
+						local axis_dir = {x=0, y=0, z=0}
+						axis_dir[axis] = 1
+						local displacement = vector.normalize(vector.subtract(placement, door.hinge.placement))
+						if not (vector.equals(displacement, axis_dir) or vector.equals(displacement, vector.multiply(axis_dir, -1))) then
+							return nil -- Misaligned hinge offset, door cannot rotate.
+						end
+					end
 				end
 				
 				can_slide_to:set_pos(test_pos, true) -- since this is part of the door, other parts of the door can slide into it
@@ -170,7 +207,7 @@ local get_door_layout = function(pos, facedir, player)
 		test_pos, _ = to_test:pop()
 	end
 	
-	if table.getn(door.hinges) == 0 then
+	if door.hinge == nil then
 		--sliding door, evaluate which directions it can go
 		door.can_slide = {top=true, bottom=true, left=true, right=true}
 		for _,door_node in pairs(door.all) do
@@ -178,12 +215,6 @@ local get_door_layout = function(pos, facedir, player)
 			door.can_slide.bottom = door.can_slide.bottom and can_slide_to:get_pos(vector.add(door_node.pos, door.directions.bottom))
 			door.can_slide.left = door.can_slide.left and can_slide_to:get_pos(vector.add(door_node.pos, door.directions.left))
 			door.can_slide.right = door.can_slide.right and can_slide_to:get_pos(vector.add(door_node.pos, door.directions.right))
-			
-			minetest.debug("slide tests", dump(door_node), dump({can_slide_to:get_pos(vector.add(door_node.pos, door.directions.top)),
-			can_slide_to:get_pos(vector.add(door_node.pos, door.directions.bottom)),
-			can_slide_to:get_pos(vector.add(door_node.pos, door.directions.left)),
-			can_slide_to:get_pos(vector.add(door_node.pos, door.directions.right))}))
-			
 		end
 	end
 	
@@ -197,10 +228,88 @@ local slide_gate = function(door, direction)
 	door.previous_move = direction
 end
 
+--------------------------------------------------------------------------
+-- Rotation
+
+local facedir_rotate = {
+	['x'] = {
+		[-1] = {[0]=4, 5, 6, 7, 22, 23, 20, 21, 0, 1, 2, 3, 13, 14, 15, 12, 19, 16, 17, 18, 10, 11, 8, 9}, -- 270 degrees
+		[1] = {[0]=8, 9, 10, 11, 0, 1, 2, 3, 22, 23, 20, 21, 15, 12, 13, 14, 17, 18, 19, 16, 6, 7, 4, 5}, -- 90 degrees
+	},
+	['y'] = {
+		[-1] = {[0]=3, 0, 1, 2, 19, 16, 17, 18, 15, 12, 13, 14, 7, 4, 5, 6, 11, 8, 9, 10, 21, 22, 23, 20}, -- 270 degrees
+		[1] = {[0]=1, 2, 3, 0, 13, 14, 15, 12, 17, 18, 19, 16, 9, 10, 11, 8, 5, 6, 7, 4, 23, 20, 21, 22}, -- 90 degrees
+	},
+	['z'] = {
+		[-1] = {[0]=16, 17, 18, 19, 5, 6, 7, 4, 11, 8, 9, 10, 0, 1, 2, 3, 20, 21, 22, 23, 12, 13, 14, 15}, -- 270 degrees
+		[1] = {[0]=12, 13, 14, 15, 7, 4, 5, 6, 9, 10, 11, 8, 20, 21, 22, 23, 0, 1, 2, 3, 16, 17, 18, 19}, -- 90 degrees
+	}
+}
+	--90 degrees CW about x-axis: (x, y, z) -> (x, -z, y)
+	--90 degrees CCW about x-axis: (x, y, z) -> (x, z, -y)
+	--90 degrees CW about y-axis: (x, y, z) -> (-z, y, x)
+	--90 degrees CCW about y-axis: (x, y, z) -> (z, y, -x)
+	--90 degrees CW about z-axis: (x, y, z) -> (y, -x, z)
+	--90 degrees CCW about z-axis: (x, y, z) -> (-y, x, z)
+local rotate_pos = function(axis, direction, pos)
+	if axis == "x" then
+		if direction < 0 then
+			return {x= pos.x, y= -pos.z, z= pos.y}
+		else
+			return {x= pos.x, y= pos.z, z= -pos.y}
+		end
+	elseif axis == "y" then
+		if direction < 0 then
+			return {x= -pos.z, y= pos.y, z= pos.x}
+		else
+			return {x= pos.z, y= pos.y, z= -pos.x}
+		end
+	else	
+		if direction < 0 then
+			return {x= -pos.y, y= pos.x, z= pos.z}
+		else
+			return {x= pos.y, y= -pos.x, z= pos.z}
+		end
+	end
+end
+local rotate_pos_displaced = function(pos, origin, axis, direction)
+	-- position in space relative to origin
+	local newpos = vector.subtract(pos, origin)
+	newpos = rotate_pos(axis, direction, newpos)
+	-- Move back to original reference frame
+	return vector.add(newpos, origin)
+end
+
+
+local rotate_door = function (door, direction)
+	local origin = door.hinge.placement
+	local axis = door.hinge.axis
+	
+	for _, door_node in pairs(door.all) do
+		origin[axis] = door_node.pos[axis]
+		if not vector.equals(door_node.pos, origin) then -- There's no obstruction if the node is literally located along the rotation axis
+			local newpos = rotate_pos_displaced(door_node.pos, origin, axis, direction)
+			local newnode = minetest.get_node(newpos)
+			local newdef = minetest.registered_nodes[newnode.name]
+			if not newdef.buildable_to then
+				return false
+			end
+		end
+	end
+	
+	for _, door_node in pairs(door.all) do
+		door_node.pos = rotate_pos_displaced(door_node.pos, origin, axis, direction)
+		door_node.node.param2 = facedir_rotate[axis][direction][door_node.node.param2]
+	end
+	return true
+end
+
+
+----------------------------------------------------------------------------------------------------
+
+
 castle_gates.trigger_gate = function(pos, node, player)
 	local door = get_door_layout(pos, node.param2, player)
-	
-	--minetest.debug(dump(door))
 	
 	if door ~= nil then
 		for _, door_node in pairs(door.all) do
@@ -208,7 +317,7 @@ castle_gates.trigger_gate = function(pos, node, player)
 		end
 		
 		local door_moved = false
-		if table.getn(door.hinges) == 0 then -- this is a sliding door
+		if door.can_slide ~= nil then -- this is a sliding door
 			if door.previous_move == "top" and door.can_slide.top then
 				slide_gate(door, "top")
 				door_moved = true
@@ -240,6 +349,20 @@ castle_gates.trigger_gate = function(pos, node, player)
 							break
 						end
 					end
+				end
+			end
+		elseif door.hinge ~= nil then -- this is a hinged door
+			if door.previous_move == "deosil" or door.previous_move == "clockwise" then
+				door_moved = rotate_door(door, 1)
+			elseif door.previous_move == "widdershins" or door.previous_move == "counterclockwise" then
+				door_moved = rotate_door(door, -1)
+			end				
+			
+			if not door_moved then
+				if door.previous_move == "deosil" or door.previous_move == "clockwise" then
+					door.previous_move = "widdershins"
+				else
+					door.previous_move = "deosil"
 				end
 			end
 		end
@@ -283,9 +406,9 @@ minetest.register_node("castle:portcullis_slot", {
 	},
 })
 
-minetest.register_node("castle:portcullis_backer", {
+minetest.register_node("castle:portcullis_slot_reverse", {
 	drawtype = "nodebox",
-	description = S("Portcullis Backer"), 
+	description = S("Portcullis Slot Reverse"), 
 	--  top, bottom, right, left, back, front. 
 	tiles = {"default_stone_brick.png"},
 	paramtype = "light",
@@ -397,26 +520,6 @@ minetest.register_craft({
 
 --------------------------------------------------------------------------------------------------------------
 
-
-minetest.register_node("castle:gate_hinge", {
-	drawtype = "nodebox",
-	description = S("Gate Door With Hinge"),
-	groups = {choppy = 1, castle_gate_hinge = 1},
-	tiles = {
-		"default_wood.png^[transformR90",
-		},
-	paramtype = "light",
-	paramtype2 = "facedir",
-		node_box = {
-		type = "fixed",
-		fixed = {
-			{-0.5, -0.5, -0.5, 0.5, 0.5, -0.25},
-			{-10/16, -4/16, -10/16, -6/16, 4/16, -6/16},
-		}
-	},
-	on_rightclick = castle_gates.trigger_gate,
-})
-
 minetest.register_node("castle:gate_panel", {
 	drawtype = "nodebox",
 	description = S("Gate Door"),
@@ -489,5 +592,28 @@ minetest.register_node("castle:gate_edge_handle", {
 		}
 	},
 	_gate_edges = {right=true},
+	on_rightclick = castle_gates.trigger_gate,
+})
+
+
+------------------------------------------------------------------------------
+
+minetest.register_node("castle:gate_hinge", {
+	drawtype = "nodebox",
+	description = S("Gate Door With Hinge"),
+	groups = {choppy = 1, castle_gate = 1},
+	tiles = {
+		"default_wood.png^[transformR90",
+		},
+	paramtype = "light",
+	paramtype2 = "facedir",
+		node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.5, -0.5, -0.5, 0.5, 0.5, -0.25},
+			{-10/16, -4/16, -10/16, -6/16, 4/16, -6/16},
+		}
+	},
+	_gate_hinge = {axis="top", offset={"front","left"}},
 	on_rightclick = castle_gates.trigger_gate,
 })
