fbcb2e018fbc38ac98b5da282c619a582a94c128
FaceDeer <derksenmobile@gmail.com>
2017-02-25 19:42:17 -0700

    weapons have been split into their own mod

--- init.lua
+++ init.lua
@@ -5,9 +5,6 @@ dofile(minetest.get_modpath("castle").."/hides.lua")
 dofile(minetest.get_modpath("castle").."/town_item.lua")
 dofile(minetest.get_modpath("castle").."/orbs.lua")
 
-dofile(minetest.get_modpath("castle").."/crossbow.lua")
-dofile(minetest.get_modpath("castle").."/battleaxe.lua")
-
 dofile(minetest.get_modpath("castle").."/workbench.lua")
 
 
