416cbad1847e75620d158251a1cdb6150fa1faab
FaceDeer <derksenmobile@gmail.com>
2017-02-25 19:56:23 -0700

    Workbench has been split into its own mod

--- init.lua
+++ init.lua
@@ -5,7 +5,3 @@ dofile(minetest.get_modpath("castle").."/hides.lua")
 dofile(minetest.get_modpath("castle").."/town_item.lua")
 dofile(minetest.get_modpath("castle").."/orbs.lua")
 
-dofile(minetest.get_modpath("castle").."/workbench.lua")
-
-
-
