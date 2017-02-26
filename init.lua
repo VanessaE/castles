f9cbc7306892bec59f7257306837361bb225dd6f
FaceDeer <derksenmobile@gmail.com>
2017-02-25 19:23:15 -0700

    lighting and storage have been moved to mods of their own

--- init.lua
+++ init.lua
@@ -1,16 +1,12 @@
 castle = {}
 
 dofile(minetest.get_modpath("castle").."/hides.lua")
-dofile(minetest.get_modpath("castle").."/lights.lua")
 
 dofile(minetest.get_modpath("castle").."/town_item.lua")
 dofile(minetest.get_modpath("castle").."/orbs.lua")
 
 dofile(minetest.get_modpath("castle").."/crossbow.lua")
 dofile(minetest.get_modpath("castle").."/battleaxe.lua")
-
-dofile(minetest.get_modpath("castle").."/ironbound_chest.lua")
-dofile(minetest.get_modpath("castle").."/crate.lua")
 
 dofile(minetest.get_modpath("castle").."/workbench.lua")
 
